d5af8ad679bf11b058df92b28f663aaed172290f
Andy Cross <andy@elastacloud.com>
2013-07-04 15:59:29 +0100

    add in the sensible files

---
+++ server/server.d.ts
@@ -0,0 +1,4 @@
+///<reference path='../typescript-node-definitions/node.d.ts'/>
+///<reference path='../typescript-node-definitions/mongodb.d.ts'/>
+///<reference path='../typescript-node-definitions/express3.d.ts'/>
+///<reference path='../typescript-node-definitions/consolidate.d.ts'/>
